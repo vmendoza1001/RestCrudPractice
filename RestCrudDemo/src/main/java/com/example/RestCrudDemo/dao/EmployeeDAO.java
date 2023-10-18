d78f31dc103c35f1c9ce66128002db41397aaaba
Vanessa Mendoza <v.mendoza1@gmail.com>
2023-10-17 22:10:36 -0400

    1. Create entity, controllers, DAO classes, service layer 2. Implement methods

---
+++ RestCrudDemo/src/main/java/com/example/RestCrudDemo/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package com.example.RestCrudDemo.dao;
+
+import com.example.RestCrudDemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
---
+++ RestCrudDemo/src/main/java/com/example/RestCrudDemo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.example.RestCrudDemo.service;
+
+import com.example.RestCrudDemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
